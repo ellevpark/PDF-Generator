f2c1b9ba105d849eb9139e713a3e4a18b427cec9
Ellen Park <ellenpark781@gmail.com>
2020-01-29 21:28:56 -0500

    added stars

--- index.js
+++ index.js
@@ -7,20 +7,7 @@ var conversion = convertFactory({
 });
 const path = require("path");
 
-var colors = {
-  green: {
-    background: "#006400",
-  },
-  blue: {
-    background: "#00BFFF",
-  },
-  pink: {
-    background: "#FFB6C1",
-  },
-  purple: {
-    background: "#9370DB",
-  }
-};
+
 // ​var username = ""; 
 // var name = ""; 
 // var profImage= ""; 
@@ -47,14 +34,10 @@ inquirer.prompt([
   }
 ])
 .then(function(response){
-    console.log(response.list)
     var favColor = response.color 
-    // if (favColor === colors[]){
-    // }
-    var userColor = response.color;
+    console.log (favColor)
     var {username} = response;
-    //TODO: COLORS 
-  var queryURL = `https://api.github.com/users/${username}`;
+    var queryURL = `https://api.github.com/users/${username}`;
   axios.get(queryURL)
   .then(function(res){
     username = res.data.login;
@@ -67,7 +50,21 @@ inquirer.prompt([
     following = res.data.following;
     repos = res.data.public_repos;
     userPage = res.data.html_url;
+    starCount = 0;
     // stars = ???? SEPARATE API CALL?? 
+
+    axios.get("https://api.github.com/users/ellevpark/repos").catch(function(error){
+      console.log("BOO! ERROR")
+    })
+    .then(function(repos){
+     
+    var repos = repos.data;
+    var starCount = 0;
+      repos.forEach(repo => {
+          starCount += repo.stargazers_count;
+        });
+        console.log (starCount)
+    });
     var generateHTML = `
     <!DOCTYPE html>
     <style>
@@ -88,34 +85,36 @@ inquirer.prompt([
       <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
       </nav>
-      <div class="container-fluid ${favColor}">
+      <div class="container-fluid color-${favColor}">
         <img class= "center circular--square" src="${profImage}" class="rounded mx-auto d-block" alt="GitHub Profile Image">
         <h1>Hi! ${name}</h1>
-        <p>${bio}</p>
+        <p id= "bio">${bio}</p>
+        <div class = "container links">
         <span><a href="https://maps.google.com/?q=${location}">${location}</a></span> <span><a href="${userPage}">GitHub</a></span>
         <span><a href="${blog}">Blog</a></span>
       </div>
+      </div>
       <div class="container">
         <div class="row row-cols-2">
-          <div class="col card text-white ${favColor} mb-3" style="max-width: 25rem;">
+          <div class="col card text-white color-${favColor} mb-3" style="max-width: 25rem;">
             <div class="card-body">
               <h5 class="card-title">Public Repositories</h5>
               <p class="card-text"> ${repos}</p>
           </div>
         </div>
-        <div class="col card text-white ${favColor} mb-3" style="max-width: 25rem;">
+        <div class="col card text-white color-${favColor} mb-3" style="max-width: 25rem;">
           <div class="card-body">
             <h5 class="card-title">Followers</h5>
             <p id= "followers" class="card-text">${followers}</p>
         </div>
       </div>
-      <div class="col card text-white ${favColor} mb-3" style="max-width: 25rem;">
+      <div class="col card text-white color-${favColor} mb-3" style="max-width: 25rem;">
         <div class="card-body">
           <h5 class="card-title">GitHub Stars</h5>
-          <p id = "stars" class="card-text"></p>
+          <p id = "stars" class="card-text">${starCount}</p>
       </div>
     </div>
-    <div class="col card text-white ${favColor} mb-3" style="max-width: 25rem;">
+    <div class="col card text-white color-${favColor} mb-3" style="max-width: 25rem;">
       <div class="card-body">
         <h5 class="card-title">Following</h5>
         <p class="card-text">${following}</p>
@@ -125,7 +124,36 @@ inquirer.prompt([
       </div>
     
       <style>
-        
+      .color-blue {
+        color: "#00BFFF";
+        -webkit-print-color-adjust:exact !important;
+
+      }
+    
+      .color-green: {
+        color: "#006400";
+        -webkit-print-color-adjust:exact !important;
+
+          },
+
+      .color-pink: {
+        color: "#FFB6C1";
+        -webkit-print-color-adjust:exact !important;
+
+          },
+      .color-purple: {
+        color: "#9370DB";
+        -webkit-print-color-adjust:exact !important;
+        }
+      .links{
+          text-align: center; 
+      }
+      span{
+          padding: 20px; 
+        }
+        #bio{
+          text-align: center; 
+        }
       nav {
         height: 5em;
         }
@@ -151,6 +179,7 @@ inquirer.prompt([
           max-width: 100%;
           height: auto;
         }
+  
     </style>
     `
     conversion({ html: generateHTML}, function(err, result) {
